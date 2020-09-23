6e6c2a14d3604476b63826ac45f6480509885545
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2020-09-23 11:50:54 -0500

    finalizando demostracion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { CodePush, InstallMode } from '@ionic-native/code-push/ngx';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private codePush: CodePush
   ) {
     this.initializeApp();
   }
@@ -22,6 +24,31 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.checkCodePush();
     });
+  }
+
+  checkCodePush() {
+    this.codePush
+      .sync(
+        {
+          updateDialog: {
+            updateTitle: "Nueva Actualización",
+            mandatoryUpdateMessage: "Existe una nueva actualización",
+            optionalUpdateMessage: "Hay una nueva versión, deseas instalarlo?",
+            optionalInstallButtonLabel: "Instalar",
+            optionalIgnoreButtonLabel: "Ignorar"
+          },
+          installMode: InstallMode.IMMEDIATE,
+        }
+      )
+      .subscribe(
+        (data) => {
+          console.log("data:: ", data);
+        },
+        (error) => {
+          console.log("error:: ", error);
+        }
+      );
   }
 }
